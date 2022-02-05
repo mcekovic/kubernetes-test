fd97109a831be20165884316d922f1fa2c605e26
Mileta Cekovic <mileta.cekovic@sbb.rs>
2022-02-05 15:06:28 +0100

    Tweaks

--- src/test/java/org/strangeforest/kubernetestest/HelloInjectorET.java
+++ src/test/java/org/strangeforest/kubernetestest/HelloInjectorET.java
@@ -1,37 +1,43 @@
 package org.strangeforest.kubernetestest;
 
 import java.io.*;
+import java.util.*;
 
 import okhttp3.*;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.parallel.*;
 import org.springframework.http.*;
+import org.springframework.web.client.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class HelloInjectorET {
 
 	private OkHttpClient client;
+	private Random rnd;
+
+	public static final int REPETITIONS = 1000;
+	private static final int HELLOS = 5;
 
 	@BeforeAll
 	void setUp() {
 		client = new OkHttpClient();
+		rnd = new Random();
 	}
 
-	@RepeatedTest(100)
+	@RepeatedTest(REPETITIONS)
 	@Execution(ExecutionMode.CONCURRENT)
-	void sayHello(RepetitionInfo repetitionInfo) throws IOException {
-		extracted("K8s-" + repetitionInfo.getCurrentRepetition() % 10);
-	}
-
-	private void extracted(final String name) throws IOException {
+	void sayHello() throws IOException {
 		var request = new Request.Builder()
-			.url("http://localhost/hello/" + name)
+			.url("http://localhost/hello/K8s-" + rnd.nextInt(HELLOS))
 			.build();
 
 		try (var response = client.newCall(request).execute()) {
 			var status = HttpStatus.valueOf(response.code());
 			var body = response.body();
-			System.out.printf("%1$s: %2$s%n", status, body != null ? body.string() : "");
+			var bodyStr = body != null ? body.string() : "";
+			if (status.isError())
+				throw new HttpServerErrorException(status, bodyStr);
+			System.out.printf("%1$s: %2$s%n", status, bodyStr);
 		}
 	}
 }
